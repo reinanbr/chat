6da5c3f03df39af7ce1c409f3f0e48d89599aef2
perseu912 <perseu912@gmail.com>
2019-07-25 20:59:17 -0300

    testando...

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("estamos online?")
+var express = require("express")
+
+var port = process.env.PORT || 8182
+var app = express()
+
+app.get('/index/:msg', (req, res) => {
+	res.status(200)
+	msg = req.params.msg
+	res.send(msg)
+	console.log(req.headers)
+})
+
+app.listen(port, () => {
+	console.log(`online na porta ${port}`)
+})
