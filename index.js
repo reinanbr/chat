770a19f08667babd22e727a8f67459a325fc90d1
perseu912 <perseu912@gmail.com>
2019-07-26 00:03:37 -0300

    testando

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express()
 const index = path.join(__dirname, "public")
 
 app.use(express.static(index))
+
+app.get('/chat/:msg', (req, res) => {
+	res.status(200)
+	console.log(req.headers)
+	msg = req.params.msg
+	res.send(msg)
+})
+
 const server = http.createServer(app)
 
 server.listen(port, () => {
