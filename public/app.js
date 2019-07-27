9852becca879f177a4789ebe5c1e662a5f9cfd67
perseu912 <perseu912@gmail.com>
2019-07-27 17:01:25 -0300

    bug resolvido

--- public/app.js
+++ public/app.js
@@ -3,13 +3,13 @@
 	var socket = io()
 	
 	$("#chat").hide()
-	$("#login").click(function() {
+	login = () => {
 		user.name = $("#msg").val()
 		socket.emit("user", user, (event) => {
 			if(event) {
 				$("#chat").fadeIn()
 				$("#msg").val('')
-				$("button").attr("id", "env")
+				$("#env").attr("onclick", "env()")
 				$("#uel").text('seje bem vindo')
 				$("#env").text(" enviar")
 				$("#msg").attr("placeholder", "digite aqui...")
@@ -18,12 +18,14 @@
 				alert('nome de usuario já esrá em uso no chat')
 			}
 		})
-	})
+	}
 			
 	
 	socket.on('connect', () => {
 		$("#on").text("você está conectado.")
 	})
+	
+	
 	
 	$("#msg").keypress(function(e) {
 		if(e.which == 13){
@@ -31,14 +33,14 @@
 		}
 	})
 	
-	$("#env").click(function() {
+	env = () => {
 		user.msg = $("#msg").val()
 	    socket.emit("chat", user)
 	    $("#msg").val('')
 	    $("#chat")
         .animate({scrollTop: $("#chat")[0]
         .scrollHeight}, 500);
-	})
+	}
 	
 	socket.on("users", (user) => {
 		$("#chat").append(`<small>${user.name} entrou na sala</small>`)
